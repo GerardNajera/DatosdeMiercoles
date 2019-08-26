6065e0e033ecc5770c45a1b263aa58016774114b
GerardNajera <54558819+GerardNajera@users.noreply.github.com>
2019-08-26 18:01:13 -0500

    Add files via upload

---
+++ Mujeres_politica.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+datos_uip <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-05-08/datos_uip.csv")
+
+summary(datos_uip)
+
+datos_uip_sinNA <- datos_uip[!is.na(datos_uip$camara),]
+sapply(datos_uip_sinNA, function(x) sum(is.na(x)))
+
+sapply(datos_uip, function(x) sum(is.na(x)))
+datos_uip_sinNA <- datos_uip[!is.na(datos_uip$porcentaje_mujeres),]
+sapply(datos_uip_sinNA, function(x) sum(is.na(x)))
+
+datos_uip_sinNA<-na.omit(datos_uip_sinNA)
+
+
+# Change box plot colors by groups
+
+attach(datos_uip_sinNA)
+
+a<-ggplot(datos_uip_sinNA, aes(x=camara, y=porcentaje_mujeres, 
+                        fill=cuota_genero)) +geom_boxplot() +labs(title="Mujeres en Política",x="Cámara", y = "Porcentaje de mujeres en esa cámara")+scale_fill_manual(values= c("#4271AE","#56B4E9"))+theme_minimal()
+
+a
+
+devtools::install_github('thomasp85/gganimate')
+library(gganimate)
+library(gifski)
+library(png)
+
+
+b<-a+transition_states(
+  camara,
+  transition_length = 2,
+  state_length = 1) +
+  enter_fade() + 
+  exit_shrink() +
+  ease_aes('sine-in-out')
+b
+anim_save("Mujeres_politica.gif",b)
+
